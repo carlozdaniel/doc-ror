0f6f01ac2171167500b38f2a8eaddc7020438da3
carlos daniel hernandez rodriguez <a20160383@gmail.com>
2021-07-23 18:17:37 -0500

    add module the photo and validates

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,9 +3,4 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
-  after_create { |admin| admin.send_reset_password_instruccion }
-
-  def password_required
-    new_record? ? false : super
-  end
 end
