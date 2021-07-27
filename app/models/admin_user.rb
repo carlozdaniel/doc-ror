23724fadf73994d2c46cc8cd529b783e09e25cad
carlos daniel hernandez rodriguez <a20160383@gmail.com>
2021-07-27 00:30:48 -0500

    view index and photos in active admin
    add model
    validations the visibility and on method writing

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,15 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+  has_many :photos
+
+  after_create { |admin| admin.send_reset_password_instructions }
+
+  def password_required?
+    new_record? ? false : super
+  end
+
+  def to_s
+    self.email
+  end
 end
