8e72fae1167bddd3de9b4efffede5c77bfb6b1f9
carlos daniel hernandez rodriguez <a20160383@gmail.com>
2021-07-28 02:11:02 -0500

    edit view photo

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   # photos controller
-  get 'photos/:id', to: 'photos#show'
+  get 'photos/:id', to: 'photos#show', as: "photo_detail"
   #welcome controller
-  get 'welcome/index', to: 'welcome#index'
+  get 'welcome/index', to: 'welcome#index', as: 'welcome_home'
 
   root to: 'welcome#index' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
